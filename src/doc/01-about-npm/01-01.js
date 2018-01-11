67cd95345b3fe2bed98a6a04386d16b803c24513
luotao <luotaoyeah@gmail.com>
2018-01-11 18:55:59 +0800

    doc: about npm

---
+++ src/doc/01-about-npm/01-01.js
@@ -0,0 +1,11 @@
+/*
+ * About NPM
+ */
+
+console.log("\n-------------------------------------------------- 01");
+/*
+ * npm 包含三个部分：
+ *     the website（https://www.npmjs.com）
+ *     the cli（npm）
+ *     the registry（https://registry.npmjs.org）
+ */
