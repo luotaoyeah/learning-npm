b17d8f32ffc5d2986a07dd2c3e71097b79e74d76
luotao <luotaoyeah@gmail.com>
2018-01-13 19:13:55 +0800

    doc: package and module: README.md

---
+++ src/doc/03-packages-and-modules/03-02-contributing-packages-to-the-registry/03-02-04.js
@@ -0,0 +1,14 @@
+/*
+ * Packages and Modules
+ *     Contributing Packages to The Registry
+ *         About Package README.md Files
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 发布到 npmjs 的包，通常都应该包含一个 README.md 文件，
+   * 该文件位于项目的根目录下面，在 npmjs 网站上该包的首页上，会展示该文件的内容，
+   * 当发布了新的版本时，首页上就会根据新的 README.md 文件内容，来更新首页展示的内容
+   */
+}
