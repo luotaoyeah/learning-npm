ecfbe90001b89ed2c054bb8dac74f0f8973a0fcf
luotao <luotaoyeah@gmail.com>
2018-01-13 18:20:47 +0800

    doc: package and module: about public and private packages

---
+++ src/doc/03-packages-and-modules/03-01-introduction-to-packages-and-modules/03-01-03.js
@@ -0,0 +1,31 @@
+/*
+ * Packages and Modules
+ *     Introduction to Packages and Modules
+ *         About Public Packages
+ *         About Private Packages
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * public package 分为两种：
+   *     unscoped public package
+   *     scoped public package
+   *
+   * scoped public package 又分为两种：
+   *     user-scoped public package
+   *     org-scoped public package
+   *
+   *
+   * unscoped public package 的包名形式为：xxx
+   * scoped public package 的包名形式为：@user/xxx，@org/xxx
+   */
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * private package 必须是 scoped private package，
+   * 包括 user-scoped private package 和 org-scoped private package
+   */
+}
