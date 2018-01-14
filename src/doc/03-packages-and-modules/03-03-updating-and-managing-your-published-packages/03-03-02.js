476ff970e732032e34d80971cf90327a6658a6f3
luotao <luotaoyeah@gmail.com>
2018-01-14 19:56:14 +0800

    doc: package and module: unpublish

---
+++ src/doc/03-packages-and-modules/03-03-updating-and-managing-your-published-packages/03-03-02.js
@@ -0,0 +1,17 @@
+/*
+ * Packages and Modules
+ *     Updating and Managing Your Published Packages
+ *         Unpublishing Packages from The Registry
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 当包发布到仓库之后，在 72 小时之内，可以取消发布，
+   * 可以取消发布整个包，也可以取消发布包的某个版本，
+   *     如果取消发布了整个包，则之后不能再重新发布这个包，除非重新换个包名，
+   *     如果取消发布了某个版本，则之后不能再重新发布这个版本，必须更新版本，
+   * 使用下面的命令：
+   *     $ npm unpublish <package>@<version>
+   */
+}
