4350f305a21b94267ce48c62f1af98f25d573193
luotao <luotaoyeah@gmail.com>
2018-01-14 19:16:12 +0800

    doc: package and module: dist-tag

---
+++ src/doc/03-packages-and-modules/03-02-contributing-packages-to-the-registry/03-02-07.js
@@ -0,0 +1,17 @@
+/*
+ * Packages and Modules
+ *     Contributing Packages to The Registry
+ *         Adding dist-tags to Packages
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 当使用 $ npm publish 命令发布包时，默认会设置它的 dist-tag 为 latest，
+   * 可以通过命令 $ npm publish --tag <sometag> 来指定其他的 dist-tag，如：
+   *     $ npm publish --tag beta
+   *
+   * 如果要给某个已经发布了的版本添加 dist-tag，可以通过下面的命令：
+   *     $ npm dist-tag add <package>@<version> <sometag>
+   */
+}
