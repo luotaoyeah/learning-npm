0759e8db1d8456a1abb0001f224976996866d9d2
luotao <luotaoyeah@gmail.com>
2018-01-15 18:48:45 +0800

    doc: package and module: update packages

---
+++ src/doc/03-packages-and-modules/03-04-getting-packages-from-the-registry/03-04-02.js
@@ -0,0 +1,33 @@
+/*
+ * Packages and Modules
+ *     Getting Packages From The Registry
+ *         Updating Packages Downloaded From The Registry
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 通过下面的命令，查看全局包中哪些包可以升级：
+   *     $ npm outdated -g --depth=0
+   *
+   * 通过下面的命令，将某个全局包更新到 latest 版本：
+   *     $ npm update -g <package>
+   *
+   * 通过下面的命令，将所有全局包更新到 latest 版本：
+   *     $ npm update -g
+   */
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 通过下面的命令，查看本地包中哪些包可以升级：
+   *     $ npm outdated
+   *
+   * 通过下面的命令，将某个本地包更新到 latest 版本：
+   *     $ npm update <package>
+   *
+   * 通过下面的命令，将所有本地包更新到 latest 版本：
+   *     $ npm update
+   */
+}
