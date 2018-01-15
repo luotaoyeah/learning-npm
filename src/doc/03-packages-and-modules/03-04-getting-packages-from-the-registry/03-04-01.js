62d7fa1ba4323ac6402159c7075b1433a504e001
luotao <luotaoyeah@gmail.com>
2018-01-15 18:25:42 +0800

    doc: package and module: install packages locally

---
+++ src/doc/03-packages-and-modules/03-04-getting-packages-from-the-registry/03-04-01.js
@@ -0,0 +1,20 @@
+/*
+ * Packages and Modules
+ *     Getting Packages From The Registry
+ *         Downloading and Installing Packages Locally
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 本次安装某个 package 使用命令：$ npm install <package>
+   * 当 <package> 后面没有指定 version 或者 tag 时，默认时使用的 latest 这个 tag，
+   * 即默认相当于：$ npm install <package>@latest
+   * 可以手动指定要安装的 version 或者 tag：
+   *     $ npm install <package>@<version>
+   *     $ npm install <package>@<tag>
+   *
+   * 可以通过命令：$ npm dist-tag ls <package> 来查看这个 package 所有的 tag
+   * 可以通过命令：$ npm view <package> versions 来查看这个 package 所有的 version
+   */
+}
