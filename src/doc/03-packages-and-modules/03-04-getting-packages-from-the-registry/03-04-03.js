ad0b2e1adc540a908be7bee84bedd83b93f19ca9
luotao <luotaoyeah@gmail.com>
2018-01-15 19:01:33 +0800

    doc: package and module: uninstall packages

---
+++ src/doc/03-packages-and-modules/03-04-getting-packages-from-the-registry/03-04-03.js
@@ -0,0 +1,24 @@
+/*
+ * Packages and Modules
+ *     Getting Packages From The Registry
+ *         Uninstalling Packages and Dependencies
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 如果要卸载本地安装的某个包，执行下面的命令：
+   *     $ npm uninstall <ackage>
+   * 这个命令有两个效果：
+   *     1. 从 node_modules 文件夹删除 package 对应的文件夹，
+   *     2. 从 dependencies 或者 devDependencies 中删除对应的 package，
+   */
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 如果要卸载全局安装的某个包，执行下面的命令：
+   *     $ npm uninstall <package> -g
+   */
+}
