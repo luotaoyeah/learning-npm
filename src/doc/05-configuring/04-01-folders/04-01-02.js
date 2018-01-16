a0836276ea82943e974c55d95a17b3f7dece28e8
luotao <luotaoyeah@gmail.com>
2018-01-16 19:32:12 +0800

    doc: config: cache, temp

---
+++ src/doc/05-configuring/04-01-folders/04-01-02.js
@@ -0,0 +1,22 @@
+/*
+ * Configuring NPM
+ *     Folder Structures Used by NPM
+ *         cache
+ *         temp
+ */
+
+console.log("\n-------------------------------------------------- 01");
+{
+  /*
+   * 可以通过命令 $ npm config get cache 查看 NPM 的缓存目录，
+   * 在 windows 平台默认的缓存目录为：%APPDATA%/npm-cache
+   */
+}
+
+console.log("\n-------------------------------------------------- 02");
+{
+  /*
+   * 可以通过命令 $ npm config get tmp 查看 NPM 的临时目录，
+   * 在 windows 平台默认的临时目录为：%TEMP%
+   */
+}
